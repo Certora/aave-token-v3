5ad56d645b515b4cb0b9c825b40a01a62ea4d75f
Tadeas Kucera <tadeas@certora.com>
2023-03-28 21:26:34 +0200

    Fix applyHarness.patch, scripts, imports in harness and Makefile

--- src/BaseAaveToken.sol
+++ src/BaseAaveToken.sol
@@ -14,12 +14,11 @@ abstract contract BaseAaveToken is Context, IERC20Metadata {
     FULL_POWER_DELEGATED
   }
 
-  // reorder fields to make hooks syntax simpler
   struct DelegationAwareBalance {
-    DelegationState delegationState;
     uint104 balance;
     uint72 delegatedPropositionBalance;
     uint72 delegatedVotingBalance;
+    DelegationState delegationState;
   }
 
   mapping(address => DelegationAwareBalance) internal _balances;
